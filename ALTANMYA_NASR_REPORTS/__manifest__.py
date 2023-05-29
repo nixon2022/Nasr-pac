7ee3726eb57c842674de8f6d813953ad217f1427
nixon2022 <111509892+nixon2022@users.noreply.github.com>
2023-05-29 14:20:39 +0300

    Add files via upload

---
+++ ALTANMYA_NASR_REPORTS/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Altanmya-Nasr Reports',
+    'version': '1.0',
+    'sequence': -200,
+    'category': 'Inventory/Purchase',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_form_view.xml',
+        'report/report_info.xml',
+        'report/purchase_nasr_report.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}

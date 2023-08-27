b2f8ff256f53bcdf03bd7ac0f0675e31fd25a296
OmarDoukmakAltanmya <130458048+OmarDoukmakAltanmya@users.noreply.github.com>
2023-08-27 13:05:42 +0300

    Update account_move.py

--- ALTANYMYA_NASR_PACKAGING/models/account_move.py
+++ ALTANYMYA_NASR_PACKAGING/models/account_move.py
@@ -16,7 +16,7 @@ class AcountMoveNasr(models.Model):
             rec.delivery_id = None
             if rec.invoice_origin:
                 deliveries = self.env['stock.picking'].search([('origin', '=', rec.invoice_origin),
-                                                               ('date_done', '=', rec.l10n_sa_delivery_date),
+                                                               ('date_done.date()', '=', rec.l10n_sa_delivery_date),
                                                                ('location_dest_id.usage', '=', 'customer')])
                 print('del', deliveries)
                 # for delivery in deliveries:

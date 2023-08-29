02288de1aed8640f0963ab6b18aef84b99896bb7
OmarDoukmakAltanmya <130458048+OmarDoukmakAltanmya@users.noreply.github.com>
2023-08-29 13:45:25 +0300

    Update stock_picking.py

--- ALTANYMYA_NASR_PACKAGING/models/stock_picking.py
+++ ALTANYMYA_NASR_PACKAGING/models/stock_picking.py
@@ -36,7 +36,7 @@ class StockPickingNasr(models.Model):
                         if move_ids:
                             # rec.lot_id_name = ''
                             for move in move_ids:
-                                lot_name ='"' + 'NASP' + move.group_id.name[-7:] + move.partial_delivery + '" '
+                                lot_name ='NASP' + move.group_id.name[-7:] + move.partial_delivery + ' '
                                 if rec.lot_id_name:
                                     rec.lot_id_name += lot_name
                                 else:

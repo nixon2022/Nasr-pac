b33cdbfdd573e829a77435943ca004c45821dc95
nixon2022 <111509892+nixon2022@users.noreply.github.com>
2023-09-27 14:19:45 +0300

    Add files via upload

--- ALTANYMYA_NASR_PACKAGING/models/mrp_bom.py
+++ ALTANYMYA_NASR_PACKAGING/models/mrp_bom.py
@@ -8,4 +8,9 @@ class MrpBomNasr(models.Model):
 
 class MrpWorkOrder(models.Model):
     _inherit = 'mrp.workorder'
+    mr = fields.Boolean(string="MR", related='operation_id.mr')
+
+
+class MrpRoutingWorkcenter(models.Model):
+    _inherit = 'mrp.routing.workcenter'
     mr = fields.Boolean(string="MR")
